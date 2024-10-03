b643cfa62f5b5021a2df87f819663f102ab7fc29
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-03 15:02:25 -0300

    Entendendo as Annotations

---
+++ src/main/java/com/example/demo/MathController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+	
+	
+	private static AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
+			method=RequestMethod.GET)
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) {
+		return 1D;
+	}
+
+}
