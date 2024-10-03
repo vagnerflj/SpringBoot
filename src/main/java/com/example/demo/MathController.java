a2314ef26dc76503cfdfb2ceed6314281873fc4f
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-03 16:21:29 -0300

    Definindo a logica da aplicacao

--- src/main/java/com/example/demo/MathController.java
+++ src/main/java/com/example/demo/MathController.java
@@ -7,18 +7,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class MathController {
-	
-	
+
+
 	private static AtomicLong counter = new AtomicLong();
-	
+
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
-			method=RequestMethod.GET)
+			method = RequestMethod.GET)
 	public Double sum(
 			@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo) {
-		return 1D;
-	}
+			@PathVariable(value = "numberTwo") String numberTwo
+	) throws  Exception{
 
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+	public Double convertToDouble(String strNumber){
+		return null;
+	}
+	private boolean isNumeric(String strNumber) {
+		return false;
+	}
 }
