fbe5e853abb99b1bc09bff8d53c295d7209c4e6a
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-04 16:11:54 -0300

    Retornando Listas de Objetos em Json

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/person")
@@ -18,10 +20,15 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return service.findAll();
+	}
 	@RequestMapping(value = "/{id}",
 			method=RequestMethod.GET,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id) throws Exception{
+	public Person findById(@PathVariable(value = "id") String id){
 		return service.findById(id);
 	}
 
--- src/main/java/com/example/demo/services/PersonServices.java
+++ src/main/java/com/example/demo/services/PersonServices.java
@@ -3,12 +3,27 @@ package com.example.demo.services;
 import com.example.demo.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 @Service
 public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll( ){
+
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<>();
+        for(int i = 0; i < 8; i++ ){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
 
     public Person findById(String id){
 
@@ -21,6 +36,16 @@ public class PersonServices {
         person.setAddress("Telemaco borba");
         person.setGender("Male");
 
+        return person;
+    }
+    private Person mockPerson(int i) {
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brasil " + i);
+        person.setGender("Male");
         return person;
     }
 
