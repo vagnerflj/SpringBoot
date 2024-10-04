aea7c313cd18454fcf669335922439d27e7f2243
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-04 14:07:31 -0300

    Refatorando o codigo

---
+++ src/main/java/com/example/demo/math/SimpleMath.java
@@ -0,0 +1,23 @@
+package com.example.demo.math;
+
+public class SimpleMath {
+
+    public Double sum(Double numberOne, Double numberTwo){
+        return numberOne + numberTwo;
+    }
+    public Double subtraction(Double numberOne, Double numberTwo){
+        return numberOne - numberTwo;
+    }
+    public Double multiplication(Double numberOne, Double numberTwo){
+        return numberOne * numberTwo;
+    }
+    public Double division(Double numberOne, Double numberTwo){
+        return numberOne / numberTwo;
+    }
+    public Double mean(Double numberOne, Double numberTwo){
+        return numberOne + numberTwo / 2;
+    }
+    public Double squareRoot(Double numberOne){
+        return Math.sqrt(numberOne);
+    }
+}
