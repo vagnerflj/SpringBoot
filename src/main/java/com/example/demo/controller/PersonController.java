a124a38ad81107966e08fb0ae1a0789dfff01d83
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-07 11:13:09 -0300

    Refatorando o Controller de pessoas

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -17,31 +17,27 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 
-	@RequestMapping(method=RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll(){
 		return service.findAll();
 	}
-	@RequestMapping(value = "/{id}",
-			method=RequestMethod.GET,
+	@GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id") Long id){
 		return service.findById(id);
 	}
-	@RequestMapping(method=RequestMethod.POST,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person create(@RequestBody Person person){
 		return service.create(person);
 	}
-	@RequestMapping(method=RequestMethod.PUT,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person update(@RequestBody Person person){
 		return service.update(person);
 	}
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) {
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
